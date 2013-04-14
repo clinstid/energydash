f19ce134d24c6bcff0e09a794d535398a2436369
Chris Linstid <clinstid@gmail.com>
2013-04-14 17:51:24 -0400

    Redo the project so I don't lose the settings.py file.

--- pip_install_required_packages.sh
+++ pip_install_required_packages.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+ENV_DIR=.env
+
+if [ ! -d $ENV_DIR ]; then
+    virtualenv $ENV_DIR
+fi
+
 PACKAGE_LIST_FILE=requirements.txt
 if [ -f  $PACKAGE_LIST_FILE ]; then
-    source .env/bin/activate && pip install -r $PACKAGE_LIST_FILE
+    source $ENV_DIR/bin/activate && pip install -r $PACKAGE_LIST_FILE
 else
     echo Missing package list file $PACKAGE_LIST_FILE.
 fi
