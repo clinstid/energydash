d4f039a318a05ddd34465d37b3b70793858be97e
Chris Linstid <clinstid@gmail.com>
2013-04-14 17:57:11 -0400

    Add some verbosity to the package install script.

--- pip_install_required_packages.sh
+++ pip_install_required_packages.sh
@@ -2,12 +2,16 @@
 ENV_DIR=.env
 
 if [ ! -d $ENV_DIR ]; then
+    echo Creating virtualenv for project.
     virtualenv $ENV_DIR
+    echo Done.
 fi
 
 PACKAGE_LIST_FILE=requirements.txt
 if [ -f  $PACKAGE_LIST_FILE ]; then
+    echo Installing required python modules.
     source $ENV_DIR/bin/activate && pip install -r $PACKAGE_LIST_FILE
+    echo Done.
 else
     echo Missing package list file $PACKAGE_LIST_FILE.
 fi
