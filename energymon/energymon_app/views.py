1d9a22067ae9ab4516de6f713a6dbc28735b647d
Chris Linstid <clinstid@gmail.com>
2013-05-08 23:51:53 -0400

    add jqplot; update view to pass json; try to use json... not there yet

--- energymon/energymon_app/views.py
+++ energymon/energymon_app/views.py
@@ -1,16 +1,19 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.core import serializers
+from django.utils.safestring import mark_safe
 
 from datetime import datetime, timedelta
 from random import randrange
 import pytz
+import json
 
 from energymon_app.models import Usage
 
 def index(request):
-    latest_usage_list = Usage.objects.order_by('time_of_reading')
     template = loader.get_template('energymon/index.html')
-    context = Context({ 'latest_usage_list': latest_usage_list })
+    usage_data = mark_safe(serializers.serialize("json", Usage.objects.order_by('time_of_reading')))
+    context = Context({ 'usage_data': usage_data })
     return HttpResponse(template.render(context))
 
 def gen(request):
