e216adfcc2c5294efe9551c1d04c6b7f5637f36c
Chris Linstid <clinstid@gmail.com>
2013-05-27 17:32:24 -0400

    More debugging

--- envir_collector.py
+++ envir_collector.py
@@ -5,46 +5,55 @@ import amqplib.client_0_8 as amqp
 
 from envir_settings import *
 
-ser = Serial(port=ENVIR_SERIAL_PORT,
-             baudrate=ENVIR_SERIAL_BAUDRATE,
-             bytesize=ENVIR_SERIAL_BYTESIZE,
-             parity=ENVIR_SERIAL_PARITY,
-             stopbits=ENVIR_SERIAL_STOPBITS,
-             timeout=ENVIR_SERIAL_TIMEOUT)
+def main():
+    print ">> Opening serial port."
+    ser = Serial(port=ENVIR_SERIAL_PORT,
+                 baudrate=ENVIR_SERIAL_BAUDRATE,
+                 bytesize=ENVIR_SERIAL_BYTESIZE,
+                 parity=ENVIR_SERIAL_PARITY,
+                 stopbits=ENVIR_SERIAL_STOPBITS,
+                 timeout=ENVIR_SERIAL_TIMEOUT)
 
-# TODO - Need to use a logger!
-output = open('envir.log', 'a', 1)
+    # TODO - Need to use a logger!
+    output = open('envir.log', 'a', 1)
 
-connection = amqp.Connection(host=ENVIR_MSG_HOST)
-channel = connection.channel()
-channel.access_request('/data', active=True, write=True)
-channel.exchange_declare(exchange=ENVIR_EXCHANGE_NAME, 
-                         type='direct',
-                         auto_delete=False,
-                         durable=True)
-channel.queue_declare(queue=ENVIR_MSG_QUEUE_NAME,
-                      durable=True,
-                      auto_delete=False)
-channel.queue_bind(queue=ENVIR_MSG_QUEUE_NAME,
-                   exchange=ENVIR_EXCHANGE_NAME)
+    print ">> Establishing AMQP connection to {}.".format(ENVIR_MSG_HOST)
+    connection = amqp.Connection(host=ENVIR_MSG_HOST)
+    print ">> Opening channel."
+    channel = connection.channel()
+    channel.access_request('/data', active=True, write=True)
+    print ">> Declaring exchange '{}'".format(ENVIR_EXCHANGE_NAME)
+    channel.exchange_declare(exchange=ENVIR_EXCHANGE_NAME, 
+                             type='direct',
+                             auto_delete=False,
+                             durable=True)
+    print ">> Declaring queue '{}'".format(ENVIR_MSG_QUEUE_NAME)
+    channel.queue_declare(queue=ENVIR_MSG_QUEUE_NAME,
+                          durable=True,
+                          auto_delete=False)
+    print ">> Binding queue to exchange."
+    channel.queue_bind(queue=ENVIR_MSG_QUEUE_NAME,
+                       exchange=ENVIR_EXCHANGE_NAME)
 
-while True:
-    try:
-        line = ser.readline()
-        if len(line) > 0:
-            output.write(line)
-            print ">> Received line: {}\n".format(line)
-            message = amqp.Message(body=line.rstrip(),
-                                   content_type='text/plain')
-            channel.basic_publish(msg=message,
-                                  exchange=ENVIR_EXCHANGE_NAME,
-                                  routing_key=ENVIR_MSG_QUEUE_NAME)
-            print ">> Queued message."
-    except KeyboardInterrupt:
-        print "Caught keyboard interrupt, exiting."
-        output.flush()
-        output.close()
-        channel.close()
-        connection.close()
-        sys.exit()
+    while True:
+        try:
+            line = ser.readline()
+            if len(line) > 0:
+                output.write(line)
+                print ">> Received line: {}\n".format(line)
+                message = amqp.Message(body=line.rstrip(),
+                                       content_type='text/plain')
+                channel.basic_publish(msg=message,
+                                      exchange=ENVIR_EXCHANGE_NAME,
+                                      routing_key=ENVIR_MSG_QUEUE_NAME)
+                print ">> Queued message."
+        except KeyboardInterrupt:
+            print "Caught keyboard interrupt, exiting."
+            output.flush()
+            output.close()
+            channel.close()
+            connection.close()
+            sys.exit()
 
+if __name__ == '__main__':
+    main()
