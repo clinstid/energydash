ef32e9c01e6ffcab7abf58da9c34f1d42721c4d8
Chris Linstid <Chris Linstid>
2013-05-30 23:43:34 -0400

    templates and stuff

--- energymon_app.py
+++ energymon_app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 import pytz
@@ -11,6 +11,7 @@ from utc_conversion import *
 from database import db_session
 from models import Usage
 from settings import *
+import settings
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(SQLITE_DATABASE_PATH)
@@ -22,7 +23,10 @@ def start_app():
     r = redis.StrictRedis(REDIS_HOST_NAME)
     count = r.zcard(REDIS_SET_NAME)
 
-    keys = r.zrange(REDIS_SET_NAME, 0, -1)
+    now = datetime.now(tz=pytz.utc)
+    end = dt_to_seconds(now)
+    start = dt_to_seconds(now + relativedelta(hours=-8))
+    keys = r.zrangebyscore(REDIS_SET_NAME, start, end)
     pipe = r.pipeline()
     for key in keys:
         pipe.hget(REDIS_HASH_NAME, key)
@@ -31,62 +35,22 @@ def start_app():
 
     print 'keys[{}], values[{}]'.format(len(keys), len(values))
 
-    #message += '<table border="1"><tr><th>Time</th><th>Watts</th></tr>\n'
+    # google chart data_table
+    #data_table = {'cols': [{'id': 'minute', 'label': 'Minute', 'type': 'string'},
+    #                       {'id': 'watts', 'label': 'Watts', 'type': 'number'}],
+    #              'rows': [] }
+    #for i in range(min(len(keys), len(values))):
+    #    data_table['rows'].append({'c': [{'v': str(seconds_to_dt(keys[i]))},
+    #                                     {'v': int(values[i])}]})
 
-    data_table = {'cols': [{'id': 'minute', 'label': 'Minute', 'type': 'string'},
-                           {'id': 'watts', 'label': 'Watts', 'type': 'number'}],
-                  'rows': [] }
-
+    # jquery flot data_table
+    data_table = []
     for i in range(min(len(keys), len(values))):
-        data_table['rows'].append({'c': [{'v': str(seconds_to_dt(keys[i]))},
-                                         {'v': int(values[i])}]})
+        data_table.append([str(seconds_to_dt(keys[i])), int(values[i])])
 
-    #for entry in entries_dict['entries']:
-    #    message += '<tr><td>{}</td><td>{}</td></tr>\n'.format(entry['start'], entry['usage'])
-
-    #message += '</table>\n'
-    
-    message = """
-      <html><head><title>energymon 1.0</title>
-      <script type="text/javascript" src="https://www.google.com/jsapi"></script>'
-      <script type="text/javascript">
-
-      // Load the Visualization API and the piechart package.
-      google.load('visualization', '1.0', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-"""
-    message += 'var data = new google.visualization.DataTable(' + json.dumps(data_table) + ', 0.6)\n'
-    message += """
-        // Create the data table.
-        // var data = new google.visualization.DataTable();
-
-        // Set chart options
-        var options = {'title':'Power Usage',
-                       'width':800,
-                       'height':600};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-"""
-
-    message += '</script>\n'
-    message += '<body>\n'
-    message += 'energymon 1.0<br/>\n'
-    message += 'Usage entries: {}\n'.format(count)
-    message += '<div id="chart_div"></div>'
-    message += '</body>\n'
-    message += '</html>\n'
-
-    return message
+    return render_template('charts.html',
+                           usage_data=data_table,
+                           num_entries=len(keys))
 
 @app.teardown_request
 def shutdown_session(exception=None):
--- settings.py
+++ settings.py
@@ -4,3 +4,4 @@ REDIS_HOST_NAME='localhost'
 REDIS_SET_NAME='energymon_set'
 REDIS_HASH_NAME='energymon_hash'
 SQLITE_DATABASE_PATH='/home/clinstid/src/energymon/energymon.sqlite3'
+DEBUG=True
